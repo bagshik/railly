30ea819493a4a19e8d51b47e8da2e3214a546b2a
Bakhshi Dashyan <bagshik@gmail.com>
2016-11-21 06:49:53 +0100

    Updated Stations view with jumbotron.

---
+++ resources/assets/js/components/Stations.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <div class="background-image"></div>
+    <div class="jumbotron">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h1>Stations</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
